9db990946b310d3fd901a3370f2a5bb7f933275a
CoderKinley <kinleypenjor313@gmail.com>
2023-03-05 18:01:57 +0530

    ji

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,5 @@
 const http = require("http");
 
-<<<<<<< HEAD
-const server = http.createServer(function(req,res){
-    res.write("hello world, welcome to our home page");
-    console.log("connected to the server");
-    console.log(req);
-    if(req.url === "/"){
-        res.write("Welcome to Home page");
-        res.end();
-    }
-    else if(req.url ==="/about"){
-        res.write("welcome to the About page");
-        res.end();
-    }
-    else{
-        res.write("404 error! Oop nothing found");
-        res.end();
-        // i have pushed it hard
-    }
-});
-
-server.listen(8080);
-=======
 let server = http.createServer(function(req,res){
     res.writeHead(200,{'connect-Type':"text/html"});
     res.write("hello world");
@@ -32,8 +10,6 @@ let server = http.createServer(function(req,res){
     else if(req.url === "/about"){
         res.write("this is the the about page");
         res.end();
->>>>>>> 622758d5b07d4023d477a7942d256c8329dbb5b4
-
     }
     else{
         res.write("404 page not found");
