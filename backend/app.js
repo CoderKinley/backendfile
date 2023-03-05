e748606c1459b7b275ef0c29c6bad90e601ea6ac
CoderKinley <kinleypenjor313@gmail.com>
2023-03-05 17:56:15 +0530

    lodash commit

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,23 @@
 const http = require("http");
 
-http.createServer(function(req,res){
-    res.writeHead(200,{'connect-Type':"text/html"});
-    res.write("hello world");
-    res.end();
-}).listen(8080);
+const server = http.createServer(function(req,res){
+    res.write("hello world, welcome to our home page");
+    console.log("connected to the server");
+    console.log(req);
+    if(req.url === "/"){
+        res.write("Welcome to Home page");
+        res.end();
+    }
+    else if(req.url ==="/about"){
+        res.write("welcome to the About page");
+        res.end();
+    }
+    else{
+        res.write("404 error! Oop nothing found");
+        res.end();
+        // i have pushed it hard
+    }
+});
+
+server.listen(8080);
 
--- backend/app1.js
+++ backend/app1.js
@@ -1,2 +1,1 @@
 const {readFileSync,writeFileSync} = require("fs");
-const first = readFileSync("./")
