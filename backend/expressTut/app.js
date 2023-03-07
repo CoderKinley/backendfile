e5c0d94c94319b6ce9df151b5940623bbf683e38
CoderKinley <kinleypenjor313@gmail.com>
2023-03-07 09:59:56 +0530

    23

--- backend/expressTut/app.js
+++ backend/expressTut/app.js
@@ -4,11 +4,25 @@ const {readFileSync} = require('fs');
 // home page 
 const homePage = readFileSync('./navBarApp/index.html');
 
-
 const server = http.createServer((req,res)=>{
-    res.write("hello");
-    res.end();
+    // url
+    const reqUrl = req.url;
+    console.log(reqUrl);
+    // looping through the url
+    if(reqUrl === "./"){
+        res.writeHead(200, {"content-type":"text/html"});
+        res.write(homePage);
+        res.end();
+    }
+    else if(reqUrl === "./about"){
+        res.writeHead(200, {"content-type":"text/html"})
+        res.end("about page");
+    }
+    else{
+        res.end("404 erro");
+    }
+
 });
-server.listen(3000,()=>{
-    console.log("connected to the local host");
+server.listen(4000,()=>{
+    console.log("connected to the local host!");
 });
