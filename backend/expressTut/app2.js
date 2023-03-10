70ddf8278ceedf2be60adcbe908b0ef19145e5fd
CoderKinley <kinleypenjor313@gmail.com>
2023-03-10 06:59:46 +0530

    app 3 adding somethi g new

--- backend/expressTut/app2.js
+++ backend/expressTut/app2.js
@@ -16,9 +16,20 @@ app.get("/api/products",(req,res)=>{
 });
 
 // to get a single product
-app.get("/api/products/1",(req,res)=>{
-    const singleProduct = products.find(product=>product.id ===1);
-    res.json(singleProduct);
+// app.get("/api/products/1",(req,res)=>{
+//     const singleProduct = products.find(product=>product.id ===1);
+//     res.json(singleProduct);
+// });
+
+// this code is using the route holder that is /:
+app.get("/api/products/:productID", (req, res)=>{
+    console.log(req.params);
+    const {productID} = req.params;
+    const requireProduct = products.find(val => val.id === Number(productID));
+    if(!requireProduct){
+        res.status(404).send("End Of The Page serach...");
+    }
+    res.json(requireProduct);
 });
 
 app.listen(5000, ()=>{
