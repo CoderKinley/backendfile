a5f91f45badf6964b7278d7b1baa6099e2b50f7e
CoderKinley <kinleypenjor313@gmail.com>
2023-03-12 18:58:43 +0530

    added the login functionality

--- backend/expressTut/app3.js
+++ backend/expressTut/app3.js
@@ -5,12 +5,15 @@ const app = express();
 //load my html file here
 app.use(express.static('./methods-public'));
 
+//parse form the data
+app.use(express.urlencoded({extended:false}));
+
 app.get("/api/people",(req,res) =>{
     res.status(200).json({success:true, status_code:200});
 });
 
-app.post("/api/people",(req,res)=>{
+app.post("/login",(req,res)=>{
     res.status(201).send("success");
-})
+});
 
 app.listen(5000,()=>console.log("connected to local host 5000...."));
