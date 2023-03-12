dd413cb085418e654c50ac83f455fdb6e0fc604b
CoderKinley <kinleypenjor313@gmail.com>
2023-03-12 18:35:14 +0530

    new afater adding the axios

--- backend/expressTut/app3.js
+++ backend/expressTut/app3.js
@@ -2,8 +2,15 @@ const express = require("express");
 const {people} = require("./data");
 const app = express();
 
+//load my html file here
+app.use(express.static('./methods-public'));
+
 app.get("/api/people",(req,res) =>{
-    res.status(200).json(people);
+    res.status(200).json({success:true, status_code:200});
 });
+
+app.post("/api/people",(req,res)=>{
+    res.status(201).send("success");
+})
 
 app.listen(5000,()=>console.log("connected to local host 5000...."));
